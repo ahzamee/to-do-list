b795583a255bdc7ecb8137ac7001e371220149d5
ahzamee <ahzamee@gmail.com>
2022-07-15 23:16:48 +0600

    build: room, dagger dependencies added

--- app/src/main/java/techetronventures/todolist/SplashScreen.kt
+++ app/src/main/java/techetronventures/todolist/SplashScreen.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
         startActivity(Intent(this, MainActivity::class.java))
         finish()
