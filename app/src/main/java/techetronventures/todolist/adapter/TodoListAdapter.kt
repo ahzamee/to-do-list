8aa7496daa4cfa35e66cf212fa6e682c5dcc6ba4
ahzamee <ahzamee@gmail.com>
2022-07-16 23:16:33 +0600

    feat: view all todo list

---
+++ app/src/main/java/techetronventures/todolist/adapter/TodoListAdapter.kt
@@ -0,0 +1,55 @@
+package techetronventures.todolist.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import techetronventures.todolist.R
+import techetronventures.todolist.database.AppEntity
+
+class TodoListAdapter(
+    private val listener: OnItemClickListener): RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder>() {
+
+    private lateinit var appEntityList: List<AppEntity>
+
+    interface OnItemClickListener {
+        fun onItemClick(data: AppEntity)
+    }
+
+    fun setTodoList(AppEntityList: List<AppEntity>?) {
+        this.appEntityList = AppEntityList!!
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_todo_item, parent, false)
+
+        return TodoListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
+        appEntityList.let { holder.bind(it[position], listener) }
+    }
+
+    override fun getItemCount(): Int {
+        if (appEntityList.size > 10){
+            return 10
+        }
+        return appEntityList.size
+    }
+
+    inner class TodoListViewHolder (view: View): RecyclerView.ViewHolder(view){
+
+        private val itemTodoTitleTxtView: TextView = view.findViewById(R.id.item_todo_title)
+        private val itemTodoDateTxtView: TextView = view.findViewById(R.id.item_todo_date)
+        //private val itemCircleView: View = view.findViewById(R.id.item_circle)
+        //private val itemLineView: View = view.findViewById(R.id.item_line)
+
+        fun bind(data: AppEntity, listener: OnItemClickListener){
+            itemTodoTitleTxtView.text = data.title
+            itemTodoDateTxtView.text = data.dateTime
+
+            itemView.setOnClickListener{listener.onItemClick(data)}
+        }
+    }
+}
--- app/src/main/java/techetronventures/todolist/view/MainActivity.kt
+++ app/src/main/java/techetronventures/todolist/view/MainActivity.kt
@@ -1,31 +1,47 @@
 package techetronventures.todolist.view
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import techetronventures.todolist.R
+import techetronventures.todolist.adapter.TodoListAdapter
+import techetronventures.todolist.database.AppEntity
 import techetronventures.todolist.util.todayDate
+import techetronventures.todolist.viewModel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoListAdapter: TodoListAdapter
     private lateinit var todayDateTxt: TextView
     private lateinit var greetingTxt: TextView
     private lateinit var additem: MaterialButton
+    private lateinit var todoListRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initVariables()
+        initTodoListRecyclerView()
 
+        loadTOdoList()
     }
 
     private fun initVariables() {
         todayDateTxt = findViewById(R.id.today_date)
         greetingTxt = findViewById(R.id.greeting)
         additem = findViewById(R.id.add_item)
+        todoListRecyclerView = findViewById(R.id.todo_list_recycler_view)
 
         todayDateTxt.text = todayDate("dd'th' MMMM")
         greetingTxt.text = resources.getText(R.string.greeting)
@@ -36,4 +52,34 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    private fun loadTOdoList() {
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        viewModel.getRecordsObserver().observe(this
+        ) { t ->
+            if (t != null) {
+                Log.d("data", "received data: " + t.size.toString())
+            }
+
+            todoListAdapter.setTodoList(t)
+            todoListAdapter.notifyDataSetChanged()
+
+        }
+
+
+    }
+
+    private fun initTodoListRecyclerView() {
+        var listener = object : TodoListAdapter.OnItemClickListener {
+            override fun onItemClick(data: AppEntity) {
+                //var intent = Intent(this@MainActivity, TodoDetails::class.java)
+                //intent.putExtra("TodoItem", data)
+                //startActivity(intent)
+                Log.d("data", data.title.toString())
+            }
+        }
+        todoListRecyclerView.layoutManager = LinearLayoutManager(this)
+        todoListAdapter = TodoListAdapter(listener)
+        todoListRecyclerView.adapter = todoListAdapter
+    }
 }
