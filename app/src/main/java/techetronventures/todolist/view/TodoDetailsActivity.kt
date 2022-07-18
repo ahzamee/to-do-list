0a449e51b479378af12b7e70fbb1613db7567ab7
ahzamee <ahzamee@gmail.com>
2022-07-18 13:39:57 +0600

    fix: create and update added in details page

--- app/src/main/java/techetronventures/todolist/view/TodoDetailsActivity.kt
+++ app/src/main/java/techetronventures/todolist/view/TodoDetailsActivity.kt
@@ -1,5 +1,6 @@
 package techetronventures.todolist.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,6 +15,8 @@ import techetronventures.todolist.viewModel.MainActivityViewModel
 
 class TodoDetailsActivity : AppCompatActivity() {
 
+    private lateinit var itemCreatedOn: TextView
+    private lateinit var itemUpdatedOn: TextView
     private lateinit var itemDateTime: TextView
     private lateinit var itemTitle: TextView
     private lateinit var itemNote: TextView
@@ -31,6 +34,8 @@ class TodoDetailsActivity : AppCompatActivity() {
     }
 
     private fun initVariables() {
+        itemCreatedOn = findViewById(R.id.item_created_on)
+        itemUpdatedOn = findViewById(R.id.item_updated_on)
         itemDateTime = findViewById(R.id.item_date_time)
         itemTitle = findViewById(R.id.item_title)
         itemNote = findViewById(R.id.item_note)
@@ -38,8 +43,12 @@ class TodoDetailsActivity : AppCompatActivity() {
         itemDelete = findViewById(R.id.item_delete)
 
         appEntity = intent.getSerializableExtra("TodoItem") as AppEntity
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
 
+        val createdOn = resources.getString(R.string.create_on)+convertDateToFormat(appEntity.createdOn.toString(),"dd:mm:yyyy", "dd'th' MMM")
+        val updatedOn = resources.getString(R.string.updated_on)+convertDateToFormat(appEntity.updatedOn.toString(),"dd:mm:yyyy", "dd'th' MMM")
+        itemCreatedOn.text = createdOn
+        itemUpdatedOn.text = updatedOn
         itemDateTime.text = convertDateToFormat(appEntity.dateTime.toString())
         itemTitle.text = appEntity.title.toString()
         itemNote.text = appEntity.note.toString()
@@ -54,8 +63,6 @@ class TodoDetailsActivity : AppCompatActivity() {
 
     private fun deleteItem() {
         viewModel.deleteItem(appEntity)
-
-
     }
 
     private fun editItem() {
